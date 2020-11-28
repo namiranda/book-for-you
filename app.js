3b4d4e524b713385a373431d3425d7bdf5c77a07
namiranda <49685356+namiranda@users.noreply.github.com>
2020-11-28 17:53:29 -0300

    agrego api y busqueda basica por titulo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    got = require('got');
 
 const app = express();
 
@@ -9,6 +10,30 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     res.render('landing');
+})
+
+app.get('/search', function(req, res) {
+    res.render('search');
+})
+
+app.get('/results', function(req, res) {
+    let query = req.query.search;
+    //let query = 'flowers'
+    let url = 'https://www.googleapis.com/books/v1/volumes?q=' + query + '&key=AIzaSyDJtSdf9wHATd58ma7SgT3lFGJybZcSCYI';
+    (async() => {
+        try {
+            const response = await got(url);
+            console.log('statusCode:', response.statusCode);
+
+
+            let data = JSON.parse(response.body);
+            res.render("results", { data: data });
+            //res.send(results['Search'][0]['Title']);
+            //res.send(data['items'][0]['volumeInfo']['title']);
+        } catch (error) {
+            console.log('error:', error);
+        }
+    })();
 })
 
 app.listen(3000);
