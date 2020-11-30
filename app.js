cfdc17de8ccf0459779d7f85279b7171bd74484f
namiranda <49685356+namiranda@users.noreply.github.com>
2020-11-30 20:25:06 -0300

    agrego random pick basado en subject, imprime titulo y autor

--- app.js
+++ app.js
@@ -18,22 +18,35 @@ app.get('/search', function(req, res) {
 
 app.get('/results', function(req, res) {
     let query = req.query.search;
-    //let query = 'flowers'
-    let url = 'https://www.googleapis.com/books/v1/volumes?q=' + query + '&key=AIzaSyDJtSdf9wHATd58ma7SgT3lFGJybZcSCYI';
+    //let url = 'http://openlibrary.org/search.json?q=' + query + '';
+    let url = 'http://openlibrary.org/subjects/' + query + '.json?limit=100';
     (async() => {
         try {
             const response = await got(url);
             console.log('statusCode:', response.statusCode);
 
-
             let data = JSON.parse(response.body);
-            res.render("results", { data: data });
-            //res.send(results['Search'][0]['Title']);
-            //res.send(data['items'][0]['volumeInfo']['title']);
+            res.render('results', { book: getRandom(data) });
         } catch (error) {
             console.log('error:', error);
         }
     })();
 })
+
+function getRandom(data) {
+    let max = data['works'].length + 1;
+    let random = Math.floor(Math.random() * max);
+    let book = {
+        title: data['works'][random]['title'],
+        authors: data['works'][random]['authors'],
+        cover_id: data['works'][random]['cover_id']
+    }
+    console.log(data['works'][random]);
+
+    console.log(book.authors[0]['name']);
+
+    return book;
+};
+
 
 app.listen(3000);
