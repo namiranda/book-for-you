9afbb090b934719aa7415a38a7c9fe2865fd94a0
namiranda <49685356+namiranda@users.noreply.github.com>
2020-12-02 20:13:56 -0300

    cambio OpenLibraryAPI por GoogleBooksAPI

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+//export const token = process.env['API_TOKEN']
+
 const express = require('express'),
     bodyParser = require('body-parser'),
     got = require('got');
 
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'));
@@ -18,7 +22,8 @@ app.get('/search', function(req, res) {
 
 app.get('/results', function(req, res) {
     let query = req.query.search;
-    let url = 'http://openlibrary.org/subjects/' + query + '.json?limit=100';
+    // let url = 'http://openlibrary.org/subjects/' + query + '.json?limit=100';
+    let url = 'https://www.googleapis.com/books/v1/volumes?q=+subject:' + query + '&key=' + process.env.API_TOKEN;
     (async() => {
         try {
             const response = await got(url);
@@ -33,17 +38,27 @@ app.get('/results', function(req, res) {
 })
 
 function getRandomBook(data) {
-    let max = data['works'].length + 1;
+    /* let max = data['works'].length + 1;
+     let random = Math.floor(Math.random() * max);
+     let book = {
+         title: data['works'][random]['title'],
+         authors: data['works'][random]['authors'],
+         cover_id: data['works'][random]['cover_id']
+     }
+     console.log(data['works'][random]);
+
+     console.log(book.authors[0]['name']);
+     */
+    let max = data['items'].length;
     let random = Math.floor(Math.random() * max);
     let book = {
-        title: data['works'][random]['title'],
-        authors: data['works'][random]['authors'],
-        cover_id: data['works'][random]['cover_id']
+        title: data['items'][random]['volumeInfo']['title'],
+        authors: data['items'][random]['volumeInfo']['authors'],
+        cover_id: data['items'][random]['volumeInfo']['imageLinks']['thumbnail']
     }
-    console.log(data['works'][random]);
+    console.log(data['items'][random]);
 
-    console.log(book.authors[0]['name']);
-
+    //console.log(book.authors[0]['name']);
     return book;
 };
 
