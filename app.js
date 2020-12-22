c1e33d6096ba168068afbe7c3d0fb0af5ea0d1a9
namiranda <49685356+namiranda@users.noreply.github.com>
2020-12-22 17:30:10 -0300

    agrego db config y user model

--- app.js
+++ app.js
@@ -1,74 +1,87 @@
-const express = require('express'),
-    bodyParser = require('body-parser'),
-    got = require('got');
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    got = require("got"),
+    mongoose = require("mongoose");
 
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
 
-app.get('/', function(req, res) {
-    res.render('landing');
-})
+mongoose.connect(process.env.DB_TOKEN, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
 
-app.get('/search', function(req, res) {
-    res.render('search');
-})
+app.get("/", function(req, res) {
+    res.render("landing");
+});
 
-app.get('/results', function(req, res) {
+app.get("/search", function(req, res) {
+    res.render("search");
+});
+
+app.get("/results", function(req, res) {
     let query = req.query.search;
     console.log(query);
-    let url = 'https://www.googleapis.com/books/v1/volumes?q=+subject:' + query + '&key=' + process.env.API_TOKEN;
+    let url =
+        "https://www.googleapis.com/books/v1/volumes?q=+subject:" +
+        query +
+        "&key=" +
+        process.env.API_TOKEN;
     (async() => {
         try {
             const response = await got(url);
-            console.log('statusCode:', response.statusCode);
+            console.log("statusCode:", response.statusCode);
 
             let data = JSON.parse(response.body);
-            if (typeof data['items'] === 'undefined') { //Si la API no devuelve una lista vacia
-                res.render('error');
+            if (typeof data["items"] === "undefined") {
+                //Si la API no devuelve una lista vacia
+                res.render("error");
             } else {
-                res.render('results', { book: getRandomBook(data) });
+                res.render("results", { book: getRandomBook(data) });
             }
-
-
         } catch (error) {
-            console.log('error:', error);
+            console.log("error:", error);
         }
     })();
-})
+});
 
 /**
  * Devuelve un objeto book, que contiene la informacion del libro seleccionado aleatoriamente
- * @param {JSON} data 
+ * @param {JSON} data
  */
 function getRandomBook(data) {
-    let max = data['items'].length;
+    let max = data["items"].length;
     let random = Math.floor(Math.random() * max);
     let book = {
-        title: data['items'][random]['volumeInfo']['title'],
-        authors: data['items'][random]['volumeInfo']['authors'],
-        description: data['items'][random]['volumeInfo']['description'],
-        cover_url: formattedCover(data['items'][random]['volumeInfo']['imageLinks']['thumbnail'])
-    }
-    console.log(data['items'][random]);
+        title: data["items"][random]["volumeInfo"]["title"],
+        authors: data["items"][random]["volumeInfo"]["authors"],
+        description: data["items"][random]["volumeInfo"]["description"],
+        cover_url: formattedCover(
+            data["items"][random]["volumeInfo"]["imageLinks"]["thumbnail"]
+        ),
+    };
+    console.log(data["items"][random]);
     console.log(book.cover_url);
     return book;
-};
+}
 
 /**
  * Devuelve un link a una imagen de portada de mayor tamaño, en caso de estar disponible
  * Ademas cambia http por https
- * @param {String} url 
+ * @param {String} url
  */
 function formattedCover(url) {
-    let index = url.indexOf('zoom=1');
-    let start = url.slice(url.indexOf('//books'), index);
-    let end = url.slice(url.indexOf('&edge'));
-    let newUrl = 'https:' + start + 'zoom=50' + end;
+    let index = url.indexOf("zoom=1");
+    let start = url.slice(url.indexOf("//books"), index);
+    let end = url.slice(url.indexOf("&edge"));
+    let newUrl = "https:" + start + "zoom=50" + end;
 
     return newUrl;
 }
---
+++ models/user.js
@@ -0,0 +1,9 @@
+const { data } = require("autoprefixer");
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+});
+
+module.exports = mongoose.model("User", UserSchema);
